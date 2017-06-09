0401b9580efc87baca4ab0226160c94815f68c86
dewijeff <jdewitt@hotmail.com>
2017-06-09 14:36:57 -0400

    yes

--- test_webcalc.py
+++ test_webcalc.py
@@ -2,6 +2,18 @@ import pytest
 from expecter import expect
 
 from webcalc import app
+from webcalc import mongo
+
+@pytest.fixture
+def pattern():
+    with app.app_context():
+        mongo.db.operations.drop()
+        mongo.db.operations.insert(
+            dict(
+                name="x",
+                pattern="{{ a * b }}"
+            )
+        )
 
 @pytest.fixture
 def client():
@@ -18,3 +30,8 @@ def describe_calc():
         response = client.get('/4/+/5')
 
         expect(response.data).contains(b"9")
+
+    def from_db(client, pattern):
+        response = client.get('/4/x/5')
+
+        expect(response.data).contains(b"20")
--- webcalc.py
+++ webcalc.py
@@ -1,6 +1,14 @@
+import os
 from flask import Flask
+from flask_pymongo import PyMongo
+from jinja2 import Template
 
 app = Flask(__name__)
+
+if os.getenv('MONGODB_URI'):
+    app.config['MONGO_URI'] = os.getenv('MONGODB_URI')
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def index():
@@ -8,7 +16,13 @@ def index():
 
 @app.route('/<int:a>/<op>/<int:b>')
 def calc(a, op, b):
-    return f"Result: {a} {op} {b} = {a + b}"
+    operation = mongo.db.operations.find_one({'name': op})
+    if operation:
+        return Template(operation['pattern']).render(a=a, b=b)
+    elif op == '+':
+        return f"Result: {a} {op} {b} = {a + b}"
+    else:
+        return f"Result: {a} {op} {b} = ???"
 
 if __name__ == '__main__':
     app.run(debug=True)
